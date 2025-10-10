93535ccd91bcc431f4615939ab114deb4ec18398
grwna <13523035@std.stei.itb.ac.id>
2025-10-10 17:31:53 +0700

    nvim skill issue

--- install.sh
+++ install.sh
@@ -1,44 +1,51 @@
 #!/bin/bash
 
 # Determine installation script's directory
-DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
+DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
+
+install_config() {
+    local name="$1"
+    local source="$2"
+    local dest="$3"
+
+    echo "Installing $name configs"
+
+    # remove old configs
+    if [ -e "$dest" ] || [ -L "$dest" ]; then
+        echo "  Removing existing config at: $dest"
+        rm -rf "$dest"
+    fi
+
+    # Create parent directory if not exist
+    mkdir -p "($dirname "$dest")"
+
+    # Create symlink
+    ln -sv "$source" "$dest"
+    echo ""
+}
 
 install_zsh() {
-    echo "Installing Zsh configs..."
-    # Example: Symlink a specific file to the home directory
-    ln -svf "$DIR/zsh" "$HOME/.config/zsh"
+    install_config "Zsh" "$DIR/zsh" "$HOME/.config"
 }
 
 install_nvim() {
-    echo "Installing Neovim configs..."
-    # Example: Symlink an entire directory to ~/.config/
-    mkdir -p "$HOME/.config"
-    ln -svf "$DIR/nvim" "$HOME/.config/nvim"
+    install_config "Neovim" "$DIR/nvim" "$HOME/.config"
 }
 
 install_hypr() {
-    echo "Installing Hyprland configs..."
-    # Example: Symlink an entire directory to ~/.config/
-    mkdir -p "$HOME/.config"
-    ln -svf "$DIR/hypr" "$HOME/.config/hypr"
+    install_config "Hyprland" "$DIR/hypr" "$HOME/.config"
 }
 
 install_waybar() {
-    echo "Installing Waybar configs..."
-    # Example: Symlink an entire directory to ~/.config/
-    mkdir -p "$HOME/.config"
-    ln -svf "$DIR/waybar" "$HOME/.config/waybar"
+    install_config "Waybar" "$DIR/waybar" "$HOME/.config"
 }
 
 install_zsh-simple() {
-    echo "Installing Zsh-simple configs..."
-    # Example: Symlink a file from this directory
-    ln -svf "$DIR/zsh-simple/.zshrc" "$HOME/.zshrc"
+    install_config "Zsh-simple" "$DIR/zsh-simple/.zshrc" "$HOME/.zshrc"
 }
 
 install_bash() {
-    echo "Installing Bash configs..."
-    ln -svf "$DIR/bash/.bashrc" "$HOME/.bashrc"
+    install_config "Bash" "$DIR/bash/.bashrc" "$HOME/.bashrc"
 }
 
 # If no arguments are provided, print usage
@@ -48,42 +55,59 @@ if [ -z "$1" ]; then
     exit 1
 fi
 
+echo "This script will install the following components: $@"
+echo "Installation is one with symlink. If the directory exists in your config folder, they will be deleted.$@"
+read -p "Do you want to proceed with the installation? [Y/n] " confirm
+
+# Check the user's response
+case "$confirm" in
+# If 'y', 'Y', or just Enter is pressed, continue
+[yY] | "")
+    echo "Starting installation..."
+    ;;
+# For any other input, abort
+*)
+    echo "Installation aborted by user."
+    exit 0
+    ;;
+esac
+
 # Loop through all provided arguments
 for component in "$@"; do
     case $component in
-        zsh)
-            install_zsh
-            ;;
-        nvim)
-            install_nvim
-            ;;
-        hypr)
-            install_hypr
-            ;;
-        waybar)
-            install_waybar
-            ;;
-        zsh-simple)
-            install_zsh-simple
-            ;;
-        bash)
-            install_bash
-            ;;
-        all)
-            install_zsh
-            install_nvim
-            install_hypr
-            install_waybar
-            install_zsh-simple
-            install_bash
-            ;;
-        *)
-            echo "Unknown component: $component"
-            ;;
+    zsh)
+        install_zsh
+        ;;
+    nvim)
+        install_nvim
+        ;;
+    hypr)
+        install_hypr
+        ;;
+    waybar)
+        install_waybar
+        ;;
+    zsh-simple)
+        install_zsh_simple
+        ;;
+    bash)
+        install_bash
+        ;;
+    all)
+        install_zsh
+        install_nvim
+        install_hypr
+        install_waybar
+        install_zsh-simple
+        install_bash
+        ;;
+    *)
+        echo "Unknown component: $component"
+        ;;
     esac
 done
 
 install_zsh-simple() {
     echo "Installing Zsh-simple..."
-    ln -s 
+    ln -s
 }
